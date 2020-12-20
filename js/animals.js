4e046fc88e52ac7c614d47bae25142d5299e27c0
Badran1987 <52219058+Badran1987@users.noreply.github.com>
2020-12-19 22:19:56 -0500

    first relase work

--- js/animals.js
+++ js/animals.js
@@ -2,8 +2,8 @@
 
 
 let fillbar = document.querySelector(".fill");
-let audios = ["../audio/dog.mp3", "../audio/caow.mp3", "../audio/cheep.mp3", "../audio/pow.mp3", "../audio/frog.mp3", "../audio/Audio_One.mp3"];
-let covers = ["../imgs/cover1.jpg", "../imgs/cover2.jpg", "../imgs/cover3.jpg"];
+let audios = ["../audio/dog.mp3", "../audio/caow.mp3", "../audio/cheep.mp3", "../audio/birds.mp3", "../audio/frog.mp3", "../audio/cat2.mp3"];
+let images = ["../images/dog.jpg","../images/cow.jpg", "../images/cheep.jpg","../images/pinyon-jay-bird.jpg", "../images/frog.jpg", "../images/cat.jpg"];
 
 let audio = new Audio();
 
@@ -19,33 +19,51 @@ function changeImage(){
 
 
 }
+
+// Create function for change images
+function imagefun() {
+    var Image_Id = document.getElementById('imgClickAndChange');
+    Image_Id.src = images[currentImage];
+    console.log( Image_Id.src, 'SRC')
+    console.log( Image_Id , 'ID')
+
+}        
+
+// Create function for change sounds.
+
 function animalsSound(elemSuond) {
 
     if (elemSuond == 'dog'){
         currentSong = 0;
         currentImage=0;
-        document.getElementById("imgClickAndChange").src == currentImage
+       
         console.log('dog', currentSong);
     }
     else if (elemSuond == 'frog') {
         currentSong = 4;
+        currentImage=4;
         console.log('frog', currentSong); 
      }
-     else if (elemSuond == 'pow') {
+     else if (elemSuond == 'cow') {
         currentSong = 1;
+        currentImage=1;
         console.log('pow', currentSong); 
      }
-  else if (elemSuond == 'hourse') {
+  else if (elemSuond == 'horse') {
     currentSong = 2;
+    currentImage=2;
     console.log('hourse', currentSong);
   }
-  else if (elemSuond == 'dove') {
+  else if (elemSuond == 'brid') {
     currentSong = 3;
+    currentImage=3;
     console.log('dove', currentSong);
   }
   else if (elemSuond == 'cat') {
     currentSong = 5;
+    currentImage=5;
     console.log('dove', currentSong);
   }
   playSong();
+  imagefun()
 }
